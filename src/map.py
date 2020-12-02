4fd0d71f5fd86196697ae5994bc6c6eb49be35db
Jaewook Ahn <ajw4586@gmail.com>
2020-12-03 06:56:27 +0900

    Fix map broken layout and core logic

--- src/map.py
+++ src/map.py
@@ -1,6 +1,6 @@
-from typing import Tuple
+from typing import List
 from PyQt5.QtWidgets import (QWidget, QGridLayout, QVBoxLayout, QLabel, QSizePolicy)
-from PyQt5.QtGui import QPalette, QPainter, QColor, QPen
+from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt, QSize
 
 BLOCK_TYPE = {
@@ -28,22 +28,127 @@ PACMAN_MAP = [
 ]
 
 
+class Block(QWidget):
+    type: int
+    painter: QPainter
+
+    def __init__(self, blockType):
+        super().__init__()
+        self.type = blockType
+        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+    def sizeHint(self):
+        return QSize(25, 25)
+
+    def paintEvent(self, event):
+        self.painter = QPainter()
+        self.painter.begin(self)
+        self.painter.eraseRect(event.rect())
+        if self.type == -1:
+            self.drawEMpty()
+        elif self.type == 0:
+            self.drawDot()
+        elif self.type == 1:
+            self.drawLWall()
+        elif self.type == 2:
+            self.drawRWall()
+        elif self.type == 3:
+            self.drawTWall()
+        elif self.type == 4:
+            self.drawBWall()
+        elif self.type == 5:
+            self.drawHWall()
+        elif self.type == 6:
+            self.drawVWall()
+        self.painter.end()
+
+    def drawEMpty(self):
+        pen = QPen(Qt.black, 3)
+        self.painter.setPen(pen)
+        self.painter.drawPoint(11, 11)
+
+    def drawDot(self):
+        pen = QPen(Qt.yellow, 3)
+        self.painter.setPen(pen)
+        self.painter.drawPoint(11, 11)
+
+    def drawHWall(self):
+        pen = QPen(Qt.white, 2, Qt.SolidLine)
+        self.painter.setPen(pen)
+        self.painter.drawLine(0, 11, 25, 11)
+
+    def drawVWall(self):
+        pen = QPen(Qt.white, 2, Qt.SolidLine)
+        self.painter.setPen(pen)
+        self.painter.drawLine(11, 0, 11, 25)
+
+    def drawTWall(self):
+        pen = QPen(Qt.white, 6, Qt.SolidLine)
+        self.painter.setPen(pen)
+        self.painter.drawLine(0, 24, 25, 24)
+
+    def drawBWall(self):
+        pen = QPen(Qt.white, 6, Qt.SolidLine)
+        self.painter.setPen(pen)
+        self.painter.drawLine(0, 1, 25, 1)
+
+    def drawLWall(self):
+        pen = QPen(Qt.white, 6, Qt.SolidLine)
+        self.painter.setPen(pen)
+        self.painter.drawLine(1, 0, 1, 25)
+
+    def drawRWall(self):
+        pen = QPen(Qt.white, 6, Qt.SolidLine)
+        self.painter.setPen(pen)
+        self.painter.drawLine(24, 0, 24, 25)
+
+
+class Cell(QWidget):
+    visited: bool
+    block: Block
+
+    def __init__(self, blockType):
+        super().__init__()
+        self.visited = False if blockType >= 0 else True
+        self.block = Block(blockType)
+        layout = QVBoxLayout()
+        layout.addWidget(self.block)
+        layout.setSpacing(0)
+        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.setLayout(layout)
+
+    def sizeHint(self):
+        return QSize(25, 25)
+
+    def visit(self):
+        if self.visited:
+            return False
+        self.visited = True
+        self.block.type = -1
+        self.block.update()
+        return True
+
+
 class GameMap(QWidget):
     mapLayout: QGridLayout
+    mapWidgets: List[List[Cell]]
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.initMap()
 
     def initMap(self):
+        self.mapWidgets = []
         container = QGridLayout()
         container.setSpacing(0)
         container.setHorizontalSpacing(0)
         container.setVerticalSpacing(0)
         for i, row in enumerate(PACMAN_MAP):
+            self.mapWidgets.append([])
             for j, col in enumerate(row):
-                block = Block(BLOCK_TYPE[col])
-                container.addWidget(block, i, j)
+                cell = Cell(BLOCK_TYPE[col])
+                container.addWidget(cell, i, j)
+                self.mapWidgets[i].append(cell)
 
         self.mapLayout = container
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
@@ -51,89 +156,15 @@ class GameMap(QWidget):
         self.setStyleSheet('background-color: #000;')
         self.show()
 
-    def isWall(self, x: int, y: int):
-        return PACMAN_MAP[x][y] != 'N'
-
-
-class Block(QWidget):
-    type: int
-
-    def __init__(self, type):
-        super().__init__()
-        self.type = type
-        layout = QVBoxLayout()
-        layout.setSpacing(0)
-        content = QLabel()
-        content.setStyleSheet('color: #fff; background-color: #fff;')
-        # layout.addWidget(content)
-        self.setLayout(layout)
-        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        self.show()
-
-    def sizeHint(self):
-        return QSize(25, 25)
-
-    def paintEvent(self, event):
-        painter = QPainter()
-        painter.begin(self)
-        if self.type == 0:
-            self.drawDot(painter)
-        elif self.type == 1:
-            self.drawLWall(painter)
-        elif self.type == 2:
-            self.drawRWall(painter)
-        elif self.type == 3:
-            self.drawTWall(painter)
-        elif self.type == 4:
-            self.drawBWall(painter)
-        elif self.type == 5:
-            self.drawHWall(painter)
-        elif self.type == 6:
-            self.drawVWall(painter)
-        painter.end()
-
-
-    def drawDot(self, painter: QPainter):
-        pen = QPen(Qt.yellow, 3)
-        painter.setPen(pen)
-        painter.drawPoint(11, 11)
-
-    def drawHWall(self, painter: QPainter):
-        pen = QPen(Qt.white, 2, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawLine(0, 11, 25, 11)
-
-    def drawVWall(self, painter: QPainter):
-        pen = QPen(Qt.white, 2, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawLine(11, 0, 11, 25)
-
-    def drawTWall(self, painter: QPainter):
-        pen = QPen(Qt.white, 6, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawLine(0, 24, 25, 24)
-
-    def drawBWall(self, painter: QPainter):
-        pen = QPen(Qt.white, 6, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawLine(0, 1, 25, 1)
-
-    def drawLWall(self, painter: QPainter):
-        pen = QPen(Qt.white, 6, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawLine(1, 0, 1, 25)
-
-    def drawRWall(self, painter: QPainter):
-        pen = QPen(Qt.white, 6, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawLine(24, 0, 24, 25)
+    def isWall(self, y: int, x: int):
+        return PACMAN_MAP[y][x] != 'N'
 
 
 if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
     import sys
     app = QApplication(sys.argv)
-    test = Map()
+    test = GameMap()
     test.setWindowTitle('Test')
     print(test.size().width(), test.size().height())
     sys.exit(app.exec_())
