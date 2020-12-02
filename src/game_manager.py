4aa84efeada34e09d76830d445cee91267e3b2c0
Jaewook Ahn <ajw4586@gmail.com>
2020-12-03 04:09:54 +0900

    Implement core game control functions

--- src/game_manager.py
+++ src/game_manager.py
@@ -1,8 +1,20 @@
+from typing import List
+
+from map import GameMap, Block, BLOCK_TYPE
+from characters import Pacman
+
 class GameManager:
     playing: bool
+    score: int
+    pacmanPos: List[int]
 
-    def __init__(self):
+    map: GameMap
+
+    def __init__(self, gameMap: GameMap = None):
         self.playing = False
+        self.score = 0
+        self.map = gameMap
+        self.pacmanPos = [9, 1]
 
     def startGame(self):
         self.playing = True
@@ -12,3 +24,27 @@ class GameManager:
 
     def isPlaying(self):
         return self.playing
+
+    def getPacmanPosition(self):
+        return tuple(self.pacmanPos)
+
+    def updatePacmanPosition(self, x: int, y: int):
+        self.pacmanPos = [x, y]
+
+    def movePacmac(self, x: int, y: int, pacman: Pacman):
+        if self.map.isWall(x, y):
+            return
+
+        oldX, oldY = self.pacmanPos
+
+        pacman = self.map.mapLayout.itemAtPosition(oldX, oldY).widget()
+        self.map.mapLayout.removeWidget(pacman)
+        emptyBlock = Block(BLOCK_TYPE['EMPTY'])
+        self.map.mapLayout.addWidget(emptyBlock, oldX, oldY)
+
+        nextBlock = self.map.mapLayout.itemAtPosition(x, y).widget()
+        self.map.mapLayout.removeWidget(nextBlock)
+        self.map.mapLayout.addWidget(pacman, x, y)
+
+        self.updatePacmanPosition(x, y)
+
