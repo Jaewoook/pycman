9b72af96b80d1092f44261b292123a8e4b0f4d1f
Jaewook Ahn <ajw4586@gmail.com>
2020-12-03 04:09:28 +0900

    Fix pacman not showing issue

--- src/characters/pacman.py
+++ src/characters/pacman.py
@@ -1,16 +1,28 @@
-from typing import Tuple
-from PyQt5.QtWidgets import QWidget
+from os import path, getcwd
+
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QSizePolicy
+from PyQt5.QtGui import QPixmap
+
 
 class Pacman(QWidget):
-    position: Tuple
+    img: QPixmap
+    imgLabel: QLabel
 
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
-        self.position = position
         self.initCharacter()
 
     def initCharacter(self):
-        pass
+        img = QPixmap(path.join(getcwd(), 'pacman.png'))
+        self.img = img.scaledToWidth(16)
+        self.imgLabel = QLabel()
+        self.imgLabel.setPixmap(self.img)
+        self.imgLabel.setStyleSheet('background: transparent;')
+        self.imgLabel.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        layout = QVBoxLayout()
+        layout.setSpacing(0)
+        layout.addWidget(self.imgLabel)
+        self.setLayout(layout)
 
-    def move(x: int, y: int):
+    def rotate(self, x: int, y: int):
         pass
